75f870d5dde51c8185292944d7fe1c12c94ec496
Mohamed_Alaa <74032994+Mohamed-Alaa-90@users.noreply.github.com>
2026-08-12 14:55:41 +0300

    setup ecommerce database and product category relationships

---
+++ app/Models/product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'is_active'
+    ];
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(category::class);
+    }
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+}
